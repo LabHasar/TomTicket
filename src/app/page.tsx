81ef3267ccad8c0a8df62c003764ea1e4fe8dd0f
Leo <140630283+LabHasar@users.noreply.github.com>
2025-02-18 14:48:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ interface Chamado {
 
 export default function Home() {
   const { data: todosChamados } = useSWR<Chamado[]>(
-    "http://api-tomticket.hasarbrasil.com.br/buscarchamado",
+    "https://api-tomticket.hasarbrasil.com.br/buscarchamado",
     fetcher,
     { refreshInterval: 5000 }
   );
